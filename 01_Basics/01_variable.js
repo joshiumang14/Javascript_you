ec287776487141da61e60e589f5a6aa13aba9080
joshiumang14 <124174591+joshiumang14@users.noreply.github.com>
2024-04-29 11:20:29 +0000

    datatype changes done

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,25 @@
+const accountId = "123456"
+let accountEmail = "Umang@gmail.com"
+var accountPassword = "321456"
+accountCity = "Jaipur"
+console.log(accountId);
+console.table([accountId, accountEmail,accountPassword, accountCity]);
+
+
+accountEmail ="palak@gmail.com"
+accountPassword = "4321"
+accountCity= "Sikar"
+let accountName;
+console.table([accountId, accountEmail,accountPassword, accountCity,accountName]);
+
+let OurName ="umang"
+console.log(OurName)
+console.log(typeof OurName);
+let convertNum = Number(OurName);
+console.log(convertNum);
+let Name ="1"
+console.log(Name)
+console.log(typeof Name);
+let convertnum = Number(Name)
+console.log(convertnum);
+console.log(typeof convertnum);
