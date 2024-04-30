aa965daf10862fbe69578d003aff6c4fc6a1647a
Umang Joshi <124174591+joshiumang14@users.noreply.github.com>
2024-04-30 12:33:16 +0000

    change

---
+++ 01_Basics/02_string.js
@@ -0,0 +1,28 @@
+const name="umang"
+const repoCount =5
+
+//console.log(name + repocount ="Value");
+// outdated ;
+
+console.log(`hello my name is ${name} and my repo count ${repoCount}`)
+const gameName =new String("UMa-ng")
+console.log(gameName[0])
+console.log(gameName.__proto__)
+console.log(gameName.length)
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(4));
+console.log(gameName.indexOf('M'));
+const newString=gameName.substring(0,4)
+console.log(newString);
+const anotherString=gameName.slice(0,4)
+console.log(anotherString);
+const anotherString1=gameName.slice(-6,2)
+console.log(anotherString1);
+const newStr="   Umang   "
+console.log(newStr);
+console.log(newStr.trim());
+const url = "https://Umang.com/hjjff"
+console.log(url.replace('https://Umang.com/hjjff','https://laughing-fiesta-j6prx54g6xr2p7r7.github.dev/'))
+console.log(url.includes('Umang'));
+console.log(gameName.split('-'));
+//jj
