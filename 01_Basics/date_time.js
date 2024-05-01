f4d3b6ca0bf30126560b3e74c8342e91e412b669
Umang Joshi <124174591+joshiumang14@users.noreply.github.com>
2024-05-01 15:50:23 +0000

    new file changes

---
+++ 01_Basics/date_time.js
@@ -0,0 +1,9 @@
+//dates
+let myDate = new Date()
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.getHours());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
+console.log(myDate.toTimeString());
---
+++ 01_Basics/number_math.js
@@ -0,0 +1,21 @@
+const score =400
+console.log(score);
+const balance= new Number(100);
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+const otherval =123.55
+console.log(otherval.toPrecision(4));
+const hundreds=1000000000
+console.log(hundreds.toLocaleString());
+console.log(hundreds.toLocaleString('en-IN'));
+
+//+++++++++ maths++++++++++
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.3));
+console.log(Math.floor(4.9));
+console.log(Math.ceil(4.9));
+console.log(Math.min(2.78,778));
+console.log(Math.max(67.998,888,67.997));
+console.log((Math.random()*10)+1);
