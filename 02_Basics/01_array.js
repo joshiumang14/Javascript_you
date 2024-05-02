ada585204edac8b5555109efe48cb195d6571b48
Umang Joshi <124174591+joshiumang14@users.noreply.github.com>
2024-05-02 13:06:12 +0000

    array part 2

--- 02_Basics/01_array.js
+++ 02_Basics/01_array.js
@@ -8,8 +8,25 @@ myArr.pop();
 // Pop use simply remove last value of array.
 
 myArr.push(9)
+//push used to put number into array
 console.log(myArr);
-myArr.unshift();
-myArr.slice();
-consol.log(myArr)
-
+myArr.unshift(5);
+// Unshife is used to add number at first please
+// myArr.shift();
+// shift is used to remove
+console.log(myArr)
+// myArr.unshift();
+console.log(myArr.includes(9))
+// includes used for check is number in the array. it return true or false.
+console.log(myArr.indexOf(4));
+const newArr=myArr.join()
+// join is used for convert into string from int.
+console.log(typeof newArr);
+const myn4= myArr.slice(3,7);
+//slice is work for range and it can't print last element of range
+console.log(myn4)
+console.log(myArr);
+const myn3= myArr.splice(3);
+// splice is used for remove used element from array and also first element = range from and last element = number of range to prt.
+console.log(myn3)
+console.log(myArr);
---
+++ 02_Basics/02_array.js
@@ -0,0 +1,30 @@
+const marvel_heros=["thor", "Ironman", "spiderman"]
+const dc_heros=["superman","flash","batman"]
+
+ marvel_heros.push(dc_heros)
+ console.log(marvel_heros);
+//Push is used for add inside into array.
+const newAll_heros=marvel_heros.concat(dc_heros)
+console.log(newAll_heros);
+//concat is used for marge all element of array 
+const new_All=[...marvel_heros, ...dc_heros]
+console.log(new_All);
+//spilt(...) is used for merge with spilt. ex-water glass...We use this method for perform work.
+const another_array=[1,2,3,4,[4,7,99,0],76,44,88,22,[6,7,9,4]]
+console.log(another_array);
+const new_another_array=another_array.flat(Infinity)
+//Flat is used for perform element in simple order 
+console.log(new_another_array);
+
+console.log(Array.isArray("Umang"));
+// to check array
+console.log(Array.from("Umang"));
+// to make array
+console.log(Array);
+console.log(Array.from({name: "joshi"}));//interesting
+// it can't make array becz ...
+let score=100
+let score1=200
+let score2=300
+console.log(Array.of(score,score1,score2));
+// instead of from we use of becz from take string and can't make array through simple elements..for more MDN 
