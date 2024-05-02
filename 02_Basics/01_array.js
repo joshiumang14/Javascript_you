45f3d3449d121640c3f40977fbcafad92c82e747
Umang Joshi <124174591+joshiumang14@users.noreply.github.com>
2024-05-02 10:32:42 +0000

    changes

--- 02_Basics/01_array.js
+++ 02_Basics/01_array.js
@@ -1,3 +1,15 @@
 //Array
 const myArr= [1,2,3,4,5,6,7]
-console.log(myArr[2]); 
+// console.log(myArr[2]); 
+const myn2= new Array(1,2,3,5)
+// console.log(myn2);
+console.log(myArr);
+myArr.pop();
+// Pop use simply remove last value of array.
+
+myArr.push(9)
+console.log(myArr);
+myArr.unshift();
+myArr.slice();
+consol.log(myArr)
+
