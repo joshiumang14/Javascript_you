c83cf25891b16866c73e34c035ab40007107b30e
Umang Joshi <124174591+joshiumang14@users.noreply.github.com>
2024-05-07 12:02:45 +0000

    change function file

--- 03_Basics/01_function.js
+++ 03_Basics/01_function.js
@@ -1,3 +1,4 @@
+/*
 function myDetails(){
     console.log("U");
     console.log("M");
@@ -31,3 +32,36 @@ function login(username){
     return `${username} just logged in `
 }
 console.log(login("umang"))
+*/
+
+function calculateCartPrice(...num){
+    return num;
+}
+console.log(calculateCartPrice(200,3345,55,55));
+
+
+//(...)=rest or spred operator.
+
+const umk={
+username:'umang',
+rollno: 32
+}
+console.log(umk);
+
+function umk1(obj){
+    console.log(`umang prints ${obj.rollno} and ${obj.username}`);}
+
+umk1(umk)// also have second method we can pass value with object argument
+
+console.log(umk1({
+    username:"prince",rollno:31}))
+//array making in function
+
+const arr= [3,4,5]
+
+function umk2(getarray){
+    console.log(`umang gives ans ${getarray[0]}`);
+}
+console.log(umk2([56,889,99,22,66]));
+
+umk2(arr)
